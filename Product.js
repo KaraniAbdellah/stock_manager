1530c03682f631571135d7c2c80091b66729db03
KaraniAbdellah <abdellahkarani@gmail.com>
2024-12-17 14:06:19 +0100

    mv function from Product.js to main file

--- Product.js
+++ Product.js
@@ -1,23 +1,12 @@
-class Product {
 
-    constructor (name, price, description, quantity, id) {
-        this.id = id;
+class Product {
+    // Constructor for create object
+    constructor (name, description, price, quantity) {
         this.name = name;
-        this.price = price;
         this.description = description;
+        this.price = price;
         this.quantity = quantity;
     }
-
-    Menu() {
-        console.log("1. Add Product");
-        console.log("2. Display All Product");
-        console.log("3. Update  Product");
-        console.log("4. Delete Product");
-        console.log("5. Exit");
-    }
-    
-
-    
 } 
 
 module.exports = Product;
--- inventory.js
+++ inventory.js
@@ -1,6 +1,8 @@
-// Import Product & prompt-sync modulo
+// Import Product & prompt-sync & File System Modulo
 const Product = require("./Product");
 const prompt = require("./packages/node_modules/prompt-sync")();
+const fs = require("fs");
+
 
 /*
     addProduct(name, description, quantity, price)
@@ -10,18 +12,59 @@ const prompt = require("./packages/node_modules/prompt-sync")();
 */
 
 
-// Decalare Variables
-let choose;
-const ProductObj = new Product("zz", 12, "dkdkd", 33);
-ProductObj.Menu();
+// Variables Decalaration
+let choose, name, description, price, quantity;
+
+// Add Product Function
+function addProduct(name, description, quantity, price) {
+    fs.readFile("application/dataProduct.json", (data, err) => {
+        // Check If There an Error
+        if (err) {
+            console.log(err); return;
+        }
+        // Get The Data From Json
+        let DataProduct = JSON.parse(data);
+        console.log(DataProduct);
+        // Add Product into the Json Object
+        DataProduct.push({
+                name: name,
+                price: price,
+                quantity: quantity,
+                description: description
+        });
+        // Convert Js Object to Json Object
+        let DataProductAsJson = JSON.stringify(DataProduct);
+        // Save Data into dataProduct.json
+        fs.writeFile("application/dataProduct.json", DataProductAsJson, err => {
+            if (err) {
+                console.log(err); return;
+            }
+            console.log("Data Saved Succffully");
+        });
+    });
+}
+
+// Method For Display The Menu
+function Menu() {
+    console.log("1. Add Product");
+    console.log("2. Display All Product");
+    console.log("3. Update  Product");
+    console.log("4. Delete Product");
+    console.log("5. Exit");
+}
 
 do {
-    ProductObj.Menu();
-    choose = paresInt(prompt("Enter Your Operations: "));
+    Menu();
+    choose = Number(prompt("Enter Your Operations: "));
 
     switch(choose) {
         case 1:
-            
+            name = prompt("Enter Product Name: ");
+            description = prompt("Enter Product Description: ");
+            price = Number(prompt("Enter Product Price: "));
+            quantity = Number(prompt("Enter Product Quantity: "));
+            console.log("Hello from inventory.js");
+            addProduct(name, description, quantity, price);
     }
 
 } while(choose != 5);
